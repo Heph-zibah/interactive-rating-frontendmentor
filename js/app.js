fda1cef6c81b75ee3a840d57572424bfeb2d4a56
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-05-27 04:56:58 +0100

    update file

--- js/app.js
+++ js/app.js
@@ -1,19 +1,50 @@
 const ratingClick = document.querySelector('#rating-click');
-const click = document.querySelectorAll('#click');
+let clicks = document.querySelectorAll('#clicks');
 const submit = document.querySelector('#submit');
 const numRate = document.querySelector('#numRate');
+const thankYouCard = document.querySelector('.thankYouCard');
+const ratingCard = document.querySelector('.ratingCard')
 
-function getSubmit() {
+let newClick = [];
+let first = document.querySelector('#clicks');
+first.classList.add('clicked');
+newClick.push(first);
 
-    console.log('hello');
-}
-
-function clickStar() {
-    for (let i = 0; i < click.length; i++) {
-        console.log(click[i]);
-    }
-}
+//ratingClick.addEventListener('click', e => {
+// if (e.target.classList.contains('click')) {
+//     e.target.classList.toggle('clicked');
+//     console.log(e.target);
+// }
+//}
 
 
-submit.addEventListener('click', getSubmit);
-ratingClick.addEventListener('click', clickStar);
+
+clicks.forEach(click => {
+    click.addEventListener('click', () => {
+        if (newClick.includes(click)) {
+            newClick[0].classList.remove('clicked');
+            newClick.pop();
+            click.classList.toggle('clicked');
+            newClick.push(click);
+        }
+
+        if (!newClick.includes(click)) {
+            newClick[0].classList.remove('clicked');
+            newClick.pop();
+            click.classList.toggle('clicked');
+            newClick.push(click);
+        }
+        const para = document.querySelectorAll('.click p');
+        console.log(para)
+        numRate.innerHTML = `You selected ${para} out of 5`;
+    })
+
+});
+
+submit.addEventListener('click', e => {
+    e.preventDefault();
+    thankYouCard.style.display = 'block';
+    ratingCard.style.display = 'none';
+
+
+});
